ad6699f3191b24084b18a6edc4cbe879b194603c
Martinaaronwu <166827155+Martinaaronwu@users.noreply.github.com>
2025-09-01 10:17:50 -0300

    a

---
+++ Assets/navigation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigation : MonoBehaviour
+
+{
+    [SerializeField] UnityEngine.AI.NavMeshAgent agent;
+    [SerializeField] Transform targetTransform;
+    [SerializeField] Animator anim;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = targetTransform.position;
+        anim.SetFloat("speed", agent.velocity.magnitude);
+    }
+}
